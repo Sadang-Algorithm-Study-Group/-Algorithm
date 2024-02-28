ee836e3c86ca97b820aaf232e072d8ab4ec131a1
= <=>
2024-02-28 19:52:51 +0900

    [BOJ16815] Star in Parentheses

--- huipyo/src/week01/boj16815/Main.java
+++ huipyo/src/week01/boj16815/Main.java
@@ -1,7 +1,38 @@
+/**
+ *  Star in Parentheses
+ *  https://www.acmicpc.net/problem/16815
+ *  [ 효율성 ]
+ *  - 메모리: 17572KB
+ *  - 시간 : 196ms
+ */
+
 package week01.boj16815;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
 
+        new LinkedList<Character>() {
+            {
+                for (char c : s.toCharArray()) {
+                    switch (c) {
+                        case '(':
+                            push(c);
+                            break;
+                        case ')':
+                            if (!isEmpty() && peek() == '(') pop();
+                            else push(c);
+                            break;
+                        case '*':
+                            System.out.println(size());
+                            break;
+                    }
+                }
+            }
+        };
+        scanner.close();
     }
 }
