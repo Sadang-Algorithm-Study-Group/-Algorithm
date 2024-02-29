7da5ef57ed79392d8a03bf27a831d424e1b3eb81
yeonkkk <tjddus0302@gmail.com>
2024-02-29 18:37:20 +0900

    김성연 1주차 스택

--- seongyeon/src/week01/boj16815/Main.java
+++ seongyeon/src/week01/boj16815/Main.java
@@ -1,7 +1,39 @@
 package week01.boj16815;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+/**
+ *  문제: Star in Parentheses (실버5)
+ *  메모리: 14192 KB
+ *  시간: 124 ms
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+
+        System.out.print(solution(s));
+    }
+
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                break;
+            }
+
+            if (c == '(') {
+                stack.push(c);
+            }
+
+            if (c == ')' & !stack.empty()) {
+                stack.pop();
+            }
+        }
+
+        return stack.size();
     }
 }
--- seongyeon/src/week01/boj17413/Main.java
+++ seongyeon/src/week01/boj17413/Main.java
@@ -1,7 +1,60 @@
 package week01.boj17413;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+/**
+ * 문제: 단어 뒤집기 2 (실버 3)
+ * 메모리 23484 kb
+ * 시간 240 ms
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+
+        System.out.print(solution(s));
+    }
+
+    public static StringBuilder solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        boolean tagOpenFlag = false;
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '<') {
+                popAll(stack, result);
+                tagOpenFlag = true;
+                result.append(ch);
+                continue;
+            }
+            else if (ch == '>') {
+                tagOpenFlag = false;
+                result.append(ch);
+                continue;
+            }
+
+            if (ch == ' ' & !tagOpenFlag) {
+                popAll(stack, result);
+                result.append(ch);
+                continue;
+            }
+
+            if (tagOpenFlag) {
+                result.append(ch);
+                continue;
+            }
+            stack.push(ch);
+        }
+        popAll(stack, result);
+        return result;
+    }
+
+    private static void popAll(Stack<Character> stack, StringBuilder result) {
+        while (!stack.empty()) {
+            result.append(stack.pop());
+        }
     }
 }
