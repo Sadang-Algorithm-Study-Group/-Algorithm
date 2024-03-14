f9b8697d4112e202502da8d167df0dbe1b68c7bf
yeonkkk <tjddus0302@gmail.com>
2024-03-14 18:06:26 +0900

    김성연 카드

---
+++ seongyeon/src/week02/boj11652/Main.java
@@ -0,0 +1,43 @@
+package week02.boj11652;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 문제: 카드
+ * 메모리: 33720 KB
+ * 시간: 352 ms
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(reader.readLine());
+
+            Map<Long, Integer> numberCounter = new HashMap<>();
+
+            long maxKey = 0L;
+            int maxValue = 0;
+            for (int i = 0; i < n; i++) {
+                long key = Long.parseLong(reader.readLine());
+                int value = numberCounter.getOrDefault(key, 0) + 1;
+                numberCounter.put(key, value);
+
+                if (maxValue < value) {
+                    maxKey = key;
+                    maxValue = value;
+                    continue;
+                }
+
+                if (maxValue == value) {
+                    maxKey = Math.min(maxKey, key);
+                }
+            }
+            System.out.println(maxKey);
+        }
+    }
+}
