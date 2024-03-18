1fd916a69e5bcfe74774dc4f96bd88e8496edf78
gyduddl <gyduddl4889@naver.com>
2024-03-18 19:35:44 +0900

    김효영3주차해시2차-3

--- hyoyoung/src/week03/boj5107/index.js
+++ hyoyoung/src/week03/boj5107/index.js
@@ -1,34 +1,35 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+let input = fs.readFileSync(filePath).toString().trim().split('\r\n');
 
-let num = 0;
-let count = 0;
+let num = 0; // 테스트 케이스 번호
+let count = 0; // 연결된 부분의 수
 
 while (input.length > 0) {
-    let graph = new Map();
-    let inputs = [];
-    let nC = Number(input.shift());
+    let graph = new Map(); // 그래프 초기화
+    let nC = Number(input.shift()); // 사람의 수
 
-    if (!isNaN(nC) && nC > 0) {
+    // 입력이 숫자이고 0보다 크면 실행
+    if (Number(nC) && !isNaN(nC)) {
+        // 그래프 구성
         for (let i = 0; i < nC; i++) {
             let readLine = input.shift().split(' ');
             let personA = readLine[0];
             let personB = readLine[1];
+            // 그래프에 해당 사람이 없으면 추가
+            if (!graph.has(readLine[0])) graph.set(readLine[0], []);
+            if (!graph.has(personB)) graph.set(personB, []);
+            console.log(graph);
 
-            if (!graph.has(personA)) {
-                graph.set(personA, []);
-            }
-            if (!graph.has(personB)) {
-                graph.set(personB, []);
-            }
-
-            graph.get(personA).push(personB);
-            graph.get(personB).push(personA);
+            // 각 사람이 선행을 베풀면 다른 사람도 선행을 베풀어야 함
+            graph.get(readLine[0]).push(personB);
+            graph.get(personB).push(readLine[0]);
         }
 
-        var visited = new Set();
+        // 방문 여부를 나타내는 Set 초기화
+        let visited = new Set();
 
+        // BFS 알고리즘 구현
         function bfs(start) {
             let queue = [start];
             while (queue.length > 0) {
@@ -41,6 +42,7 @@ while (input.length > 0) {
             }
         }
 
+        // 그래프 순회하면서 연결된 부분의 수를 찾음
         for (let [person] of graph) {
             if (!visited.has(person)) {
                 bfs(person);
@@ -48,7 +50,8 @@ while (input.length > 0) {
             }
         }
 
+        // 결과 출력
         console.log(`${++num} ${count}`);
-        count = 0;
+        count = 0; // 연결된 부분 수 초기화
     }
 }
