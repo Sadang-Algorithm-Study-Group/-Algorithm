ab87697a06fd3840edcc62e32f3b9f1a24d66326
gyduddl <gyduddl4889@naver.com>
2024-03-18 19:39:24 +0900

    김효영3주차해시-4

--- hyoyoung/src/week03/boj5107/index.js
+++ hyoyoung/src/week03/boj5107/index.js
@@ -2,56 +2,55 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\r\n');
 
-let num = 0; // 테스트 케이스 번호
-let count = 0; // 연결된 부분의 수
+let num = 0;
+let count = 0;
 
-while (input.length > 0) {
-    let graph = new Map(); // 그래프 초기화
-    let nC = Number(input.shift()); // 사람의 수
+while (input.length) {
+    let graph = new Map();
+    let inputs = [];
+    let nC = Number(input.shift());
 
-    // 입력이 숫자이고 0보다 크면 실행
     if (Number(nC) && !isNaN(nC)) {
-        // 그래프 구성
-        for (let i = 0; i < nC; i++) {
-            let readLine = input.shift().split(' ');
-            let personA = readLine[0];
-            let personB = readLine[1];
-            // 그래프에 해당 사람이 없으면 추가
-            if (!graph.has(readLine[0])) graph.set(readLine[0], []);
-            if (!graph.has(personB)) graph.set(personB, []);
-            console.log(graph);
-
-            // 각 사람이 선행을 베풀면 다른 사람도 선행을 베풀어야 함
-            graph.get(readLine[0]).push(personB);
-            graph.get(personB).push(readLine[0]);
+        inputs = inputArr(nC);
+        num++;
+        for (let array of inputs) {
+            graph.set(array[0], []);
+        }
+        for (let array of inputs) {
+            graph.get(array[0]).push(array[1]);
+            graph.get(array[1]).push(array[0]);
         }
 
-        // 방문 여부를 나타내는 Set 초기화
-        let visited = new Set();
+        var visitied = new Set();
 
-        // BFS 알고리즘 구현
         function bfs(start) {
             let queue = [start];
             while (queue.length > 0) {
                 let current = queue.shift();
                 for (let next of graph.get(current)) {
-                    if (visited.has(next)) continue;
-                    visited.add(next);
+                    if (visitied.has(next)) continue;
+                    visitied.add(next);
                     queue.push(next);
                 }
             }
         }
-
-        // 그래프 순회하면서 연결된 부분의 수를 찾음
-        for (let [person] of graph) {
-            if (!visited.has(person)) {
-                bfs(person);
+        for (let i of inputs) {
+            let name = i[0];
+            if (!visitied.has(name)) {
+                bfs(name);
                 count++;
             }
         }
-
-        // 결과 출력
-        console.log(`${++num} ${count}`);
-        count = 0; // 연결된 부분 수 초기화
+        console.log(`${num} ${count}`);
+        count = 0;
     }
+}
+
+function inputArr(nC) {
+    let inputs = [];
+    for (let i = 0; i < nC; i++) {
+        let readLine = input.shift().split(' ');
+        inputs.push(readLine);
+    }
+    return inputs;
 }
