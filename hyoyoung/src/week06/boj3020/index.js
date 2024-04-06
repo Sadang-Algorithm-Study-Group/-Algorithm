c719033d35dddef5c8cc758fc22f9219c93f4aff
gyduddl <gyduddl4889@naver.com>
2024-04-07 00:57:47 +0900

    김효영6주차누적합2차

--- hyoyoung/src/week06/boj3020/index.js
+++ hyoyoung/src/week06/boj3020/index.js
@@ -1,28 +1,33 @@
-// 개똥벌레 => 시간 초과
+// 개똥벌레
 
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\r\n');
 
-let [N, M] = input.shift().split(' ').map(Number);
+let [N, M] = input[0].split(' ').map(Number);
 
-let arr = input.map((x) => Number(x));
+let arr = input.slice(1, input.length).map(Number);
 
-let psum = Array.from({ length: M }, () => 0);
+let top = Array.from({ length: M + 1 }).fill(0);
+let bottom = Array.from({ length: M + 1 }).fill(0);
 
 arr.forEach((a, i) => {
-    if (i % 2) {
-        for (let j = 0; j < a; j++) {
-            psum[j]++;
-        }
-    } else {
-        for (let j = M - 1; j > M - 1 - a; j--) {
-            psum[j]++;
-        }
-    }
+    i % 2 ? top[M - a + 1]++ : bottom[a]++;
 });
 
-let min = Math.min(...psum);
+for (let i = 1; i <= M; i++) {
+    top[i] += top[i - 1];
+    bottom[M - i] += bottom[M - i + 1];
+}
+let min = Infinity;
+let count = 0;
 
-let count = psum.filter((x) => x === min).length;
+for (let i = 1; i <= M; i++) {
+    if (top[i] + bottom[i] < min) {
+        count = 1;
+        min = top[i] + bottom[i];
+    } else if (top[i] + bottom[i] === min) {
+        count++;
+    }
+}
 console.log(min, count);
