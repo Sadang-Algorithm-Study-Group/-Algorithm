b909543ad220ec80bf2543549821c85d21d7012f
yeonkkk <tjddus0302@gmail.com>
2024-04-08 13:51:43 +0900

    김성연 7주차 - 로프

---
+++ seongyeon/src/week07/boj2217/Main.java
@@ -0,0 +1,40 @@
+package week07.boj2217;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 문제: 로프
+ * 메모리: 25968 KB
+ * 시간: 340 ms
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(reader.readLine());
+            int[] weights = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                weights[i] = Integer.parseInt(reader.readLine());
+            }
+
+            System.out.println(solution(n, weights));
+        }
+    }
+
+    private static int solution(int n, int[] weights) {
+        Arrays.sort(weights);
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int weight = weights[i] * (n - i);
+            max = Math.max(max, weight);
+        }
+
+        return max;
+    }
+}
