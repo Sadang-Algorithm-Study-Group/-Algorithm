ab01afd894b4dffbe6374646f4b03df837c1a956
= <=>
2024-04-18 21:53:26 +0900

    [PGS43238] 입국심사(틀린답 나옴)

---
+++ huipyo/src/week08/pgs43238/Solution.java
@@ -0,0 +1,25 @@
+package week08.pgs43238;
+
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = Long.MAX_VALUE;
+        int left = 1;
+        int right = n * times[times.length - 1];
+
+        Arrays.sort(times);
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int total = Arrays.stream(times).map(time -> mid / time).sum();
+
+            if (total >= n) {
+                answer = Math.min(answer, mid);
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return answer;
+    }
+}
