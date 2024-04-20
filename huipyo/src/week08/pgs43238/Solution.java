48ad4a5744840c5f1e6ab3fc71f626d06f7927bf
= <=>
2024-04-20 23:12:52 +0900

    [PGS43238] 입국심사 수정

--- huipyo/src/week08/pgs43238/Solution.java
+++ huipyo/src/week08/pgs43238/Solution.java
@@ -4,20 +4,24 @@ import java.util.Arrays;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = Long.MAX_VALUE;
-        int left = 1;
-        int right = n * times[times.length - 1];
-
         Arrays.sort(times);
+        return getAnswer(n, times);
+    }
+
+    private long getAnswer(int n, int[] times) {
+        long answer = Long.MAX_VALUE;
+        long left = 1;
+        long right = (long) n * times[times.length - 1];
+
         while (left <= right) {
-            int mid = (left + right) / 2;
-            int total = Arrays.stream(times).map(time -> mid / time).sum();
+            long mid = (left + right) >> 1;
+            long total = Arrays.stream(times).mapToLong(time -> mid / time).sum();
 
             if (total >= n) {
                 answer = Math.min(answer, mid);
-                left = mid + 1;
-            } else {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return answer;
